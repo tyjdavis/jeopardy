1d4075d598a41ba374dd83b0d65e765aeb5884a4
Tyler Davis <tyjdavis@outlook.com>
2017-04-07 15:23:33 -0400

    updating index.js

--- index.js
+++ index.js
@@ -1,53 +1,84 @@
+function MultipleChoiceQuestion (text, answer, category, value) {
+  this.text = text;
+  this.answer = answer;
+  this.category = category;
+  this.value = value;
 
-let objectBuffer = {}; //
+
+
+this.displayCategory = function () {
+    let source = document.querySelector('#categories').innerHTML;
+    let template = Handlebars.compile(source);
+    let html = template(this);
+    document.querySelector('#categoryOnly').insertAdjacentHTML('beforeend', html);
+    document.querySelector('#categoryOnly .catSelection:last-of-type .cat').addEventListener('click', this.displayQuestion.bind(this));
+  }
+
+this.displayQuestion = function () {
+  //document.getElementById("categories").style.visibilty = "hidden";
+  console.log(this);
+  let test1 = document.querySelector('#categoryOnly');
+  test1.style.visibility = 'hidden';
+  let source = document.querySelector('#questions').innerHTML;
+  let template = Handlebars.compile(source);
+  let html = template(this);
+  document.querySelector('#questionOnly').insertAdjacentHTML('beforeend', html);
+  document.querySelector('#questionOnly .questionSelection:last-of-type #submit').addEventListener('click', this.isCorrect.bind(this));
+
+}
+
+this.isCorrect = function (event) {
+  let space = document.getElementById("text").value;
+  let li = event.target;
+  let answerSpace = li.nextElementSibling;
+  let newQuestion1 = document.querySelector('#new');
+  console.log('test');
+  if (space === this.answer) {
+    answerSpace.textContent = "Correct";
+  } else {
+    answerSpace.textContent = "Incorrect";
+  }
+}
+
+
+
+}
+
+
 
 
 fetch('http://jservice.io/api/random?count=3')
 .then(response => response.json())
-.then(displayCategory)
-//.then(displayQuestion)
-//.then(clickEvent)
+.then(getCategoriesFromAPI)
+.then(displayQuestion)
 
 
-function displayCategory (arr) {
-  return arr.map((object) => {
-    objectBuffer = object;
-    let source = document.querySelector('#categories').innerHTML;
-    let template = Handlebars.compile(source);
-    let html = template(object);
-    document.querySelector('#categoryOnly').insertAdjacentHTML('beforeend', html);
-    document.querySelector('.catSelection').addEventListener('click', displayQuestion);
-  })
+function getCategoriesFromAPI (object) {
+  return object.map(function (object) {
+  return newQuestion = new MultipleChoiceQuestion(object.question, object.answer, object.category.title, object.value)
+})
 }
 
 
-function displayQuestion () {
-    let source = document.querySelector('#questions').innerHTML;
-    let template = Handlebars.compile(source);
-    let html = template(source);
-    document.querySelector('#questionOnly').insertAdjacentHTML('beforeend', html);
-    //document.querySelector('#submit').addEventListener('click', isCorrect);
-    //console.log(source);
+
+
+function displayQuestion (arr) {
+  arr.forEach(question => question.displayCategory()); //running display from line 38
 }
-//
-// let score = 0;
-//
-// function isCorrect (event) {
-//   let space = document.getElementById("text").value;
-//   let li = event.target;
-//   let scorespace = document.querySelector('.yourScore');
-//   let answerSpace = li.nextElementSibling;
-//   if (space === objectBuffer.answer) {
-//     answerSpace.textContent = "Correct";
-//     scorespace.textContent = score + 10;
-//   } else {
-//     answerSpace.textContent = "Incorrect";
-//     scorespace.textContent = score - 10;
-//   }
-// }
-//
-//
-// function clickEvent (){
-// document.querySelector('.catSelection').addEventListener('click', clickCategory);
-// }
-//
+
+
+  // let score = 100;
+  //
+  //   MultipleChoiceQuestion.prototype.isCorrect = function (event) {
+  //     let li = event.target;
+  //     let answerSpace = li.parentElement.nextElementSibling;
+  //     let scorespace = document.querySelector('.yourScore');
+  //     if (li.textContent === this.answer) {
+  //       answerSpace.textContent = "Correct";
+  //       scorespace.textContent = score;
+  //     } else {
+  //       answerSpace.textContent = "Wrong";
+  //       score -= 10;
+  //       scorespace.textContent = score;
+  //     }
+  //   }
