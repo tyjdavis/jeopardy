e2fbcfef5d9df93fc7ed1ade8d3b83b286bf9ca0
Tyler Davis <tyjdavis@outlook.com>
2017-04-07 13:29:05 -0400

    refactoring with index2.js

--- index.js
+++ index.js
@@ -4,43 +4,50 @@ let objectBuffer = {}; //
 
 fetch('http://jservice.io/api/random?count=3')
 .then(response => response.json())
-.then(displayQuestion)
+.then(displayCategory)
+//.then(displayQuestion)
+//.then(clickEvent)
 
 
-
-function displayQuestion (arr) {
-  let display = arr.map((object) => {
+function displayCategory (arr) {
+  return arr.map((object) => {
     objectBuffer = object;
-    let source = document.querySelector('#shortAnswer').innerHTML;
+    let source = document.querySelector('#categories').innerHTML;
     let template = Handlebars.compile(source);
     let html = template(object);
-    document.querySelector('#quiz').insertAdjacentHTML('beforeend', html);
-    document.querySelector('#submit').addEventListener('click', isCorrect);
-    console.log(objectBuffer);
+    document.querySelector('#categoryOnly').insertAdjacentHTML('beforeend', html);
+    document.querySelector('.catSelection').addEventListener('click', displayQuestion);
   })
-  return display;
-  //Promise.all(display).then(isCorrect);
 }
 
-let score = 0;
 
-function isCorrect (event) {
-  let space = document.getElementById("text").value;
-  let li = event.target;
-  let scorespace = document.querySelector('.yourScore');
-  let answerSpace = li.nextElementSibling;
-  if (space === objectBuffer.answer) {
-    answerSpace.textContent = "Correct";
-    scorespace.textContent = score + 10;
-  } else {
-    answerSpace.textContent = "Incorrect";
-    scorespace.textContent = score - 10;
-  }
+function displayQuestion () {
+    let source = document.querySelector('#questions').innerHTML;
+    let template = Handlebars.compile(source);
+    let html = template(source);
+    document.querySelector('#questionOnly').insertAdjacentHTML('beforeend', html);
+    //document.querySelector('#submit').addEventListener('click', isCorrect);
+    //console.log(source);
 }
-
-let test1 = document.querySelector('.catSelection')
-test1.addEventListener('click', clickCategory);
-
-function clickCategory () {
-  console.log("test");
-}
+//
+// let score = 0;
+//
+// function isCorrect (event) {
+//   let space = document.getElementById("text").value;
+//   let li = event.target;
+//   let scorespace = document.querySelector('.yourScore');
+//   let answerSpace = li.nextElementSibling;
+//   if (space === objectBuffer.answer) {
+//     answerSpace.textContent = "Correct";
+//     scorespace.textContent = score + 10;
+//   } else {
+//     answerSpace.textContent = "Incorrect";
+//     scorespace.textContent = score - 10;
+//   }
+// }
+//
+//
+// function clickEvent (){
+// document.querySelector('.catSelection').addEventListener('click', clickCategory);
+// }
+//
